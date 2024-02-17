a6915ea18edf7f4fe346041bd6755b17c0c47f64
Jonathan Brown <jbrown@acuity.social>
2024-02-17 15:52:18 +0700

    Use Tokio.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use bip39::Mnemonic;
 use itertools::Itertools;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let words = 24;
     let mnemonic = Mnemonic::generate(words).unwrap();
     println!("{}", mnemonic.word_iter().join(" "));
